89251073d14ed2829c15c5bbdbb96849147c37da
นะ' นุ <59477859+nuttaylove4321@users.noreply.github.com>
2023-12-11 03:11:14 +0700

    Create move.php

---
+++ move.php
@@ -0,0 +1,8 @@
+{
+  "Boolean": false,
+  "GooglePlay": "https://play.google.com/store/apps/details?id=com.publicunc.uncvpn",
+  "Message": "โปรดอ่านรายละเอียด
+
+แอปพลิเคชั่นปิดปรับปรุงชั่วคราว
+กรุณากดติดตั้งเพื่อย้ายไปยังแอปพลิเคชั่นตัวใหม่"
+}
